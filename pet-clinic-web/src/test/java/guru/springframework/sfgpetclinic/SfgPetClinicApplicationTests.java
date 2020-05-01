92592ec2df4007c580d95f3770666a164e7c5716
nate <clevelandsun@gmail.com>
2020-05-01 17:39:12 -0400

    sample fix

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,6 +1,5 @@
-package sfgpetclinic;
+package guru.springframework.sfgpetclinic;
 
-import guru.springframework.sfgpetclinic.SfgPetClinicApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
