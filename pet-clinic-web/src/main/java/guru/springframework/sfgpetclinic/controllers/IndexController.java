7b604e9838c914a6d935f2d4da885516cb4a4012
nate <clevelandsun@gmail.com>
2020-05-03 16:46:15 -0400

    Added index page and controller. closes #8.

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(Model model) {
+        model.addAttribute("greeting", "Pet Clinic Index Page");
+
+        return "index";
+    }
+}
