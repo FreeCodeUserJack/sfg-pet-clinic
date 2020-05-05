326233fa2bf6ea5cd66278eedcfb7ec38ccbbd48
nate <clevelandsun@gmail.com>
2020-05-04 20:42:28 -0400

    Added Spring Config for bootstrap Dataloader. Closes #22

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// dont' need @ComponentScane() even though services/maps in another module b/c both are guru.springframework.sfgclinic
 public class SfgPetClinicApplication {
 
     public static void main(String[] args) {
